60ac5c5d491bedfe875b4c523711d29fdf0fa8f0
Mike Kreuzer <mike@mikekreuzer.com>
2016-07-07 22:52:12 +1000

    test of delta column

--- javascripts/test.js
+++ javascripts/test.js
@@ -64,6 +64,6 @@ function checkHistory(name, index, historicalData) {
   } else if(index > oldIndex) {
     return '<img src="images/down.png" alt="down" width="48" height="32" style="margin-top:-9px;margin-bottom:-15px;padding:0"> from ' + (oldIndex + 1);
   } else {
-    return historicalData.dateScraped; // '&nbsp;';
+    return 'old' + historicalData.dateScraped; // '&nbsp;';
   }
 }
